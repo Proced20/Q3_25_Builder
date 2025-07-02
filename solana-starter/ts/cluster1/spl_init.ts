8928f458544b54efbbba93ee4a0b1bbfa665179f
Elly Aster <asterelly@gmail.com>
2025-07-02 19:04:57 -0400

    updated airdrop files

--- solana-starter/ts/cluster1/spl_init.ts
+++ solana-starter/ts/cluster1/spl_init.ts
@@ -1,5 +1,5 @@
 import { Keypair, Connection, Commitment, SystemProgram, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
-import { createMint, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token';
+import { createInitializeMintInstruction, createInitializeAccount2Instruction, createMint, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token';
 import wallet from "./wallet/turbin-wallet.json"
 
 
@@ -15,60 +15,82 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
         // Start here
         // const mint = ???
 
-        //create mint address
-        const mintKey = Keypair.generate();
+        //create mint address using createMint()
+        createMintWrapped();
 
-        const connection = new Connection("https://api.devnet.solana.com");
 
-        const mint = await createMint(
-            connection,
-            keypair,
-            keypair.publicKey,
-            null,
-            9,
 
-        )
-
-        console.log(`Mint address: ${mint.toBase58()}`);
-
-        /*
-        const createAccountInstruction = SystemProgram.createAccount(
-            {
-                fromPubkey: keypair.publicKey,
-                newAccountPubkey: mintKey.publicKey,
-                space: MINT_SIZE,
-                lamports: await getMinimumBalanceForRentExemptMint(connection),
-                programId: TOKEN_2022_PROGRAM_ID
-            }
-        );
-
-        const mintInstrust = createMint(
-            connection,
-            keypair,
-            keypair.publicKey,
-            keypair.publicKey,
-            9, 
-            mintKey
-        )
-
-      
-
-        const transaction = new Transaction().add(
-            createAccountInstruction,
-            mintInstrust
-        );
-
-        const signature = await sendAndConfirmTransaction(
-            connection,
-            transaction,
-            [keypair, mint]
-        );
-
-        console.log("Mint Address: ", mint.publicKey.toBase58());
-        console.log("Transaction Signature: ", signature);
-        */
+       //Unwrapped method of CreateMint() via creating transactions
+        createMintUnwrapped();
        
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
 })()
+
+
+async function createMintWrapped(){
+    const mintKey = Keypair.generate();
+
+    const connection = new Connection("https://api.devnet.solana.com");
+
+    
+    let start = +new Date();
+    const mint = await createMint(
+        connection,
+        keypair,
+        keypair.publicKey,
+        null,
+        9,
+
+    )
+    let end = +new Date();
+
+    console.log(`Time to run: ${end - start}`);
+
+    console.log(`Mint address: ${mint.toBase58()}`); 
+
+}
+
+async function createMintUnwrapped(){
+
+    let mintKey = Keypair.generate();
+
+    let start = +new Date(); 
+    const createAccountInstruction = SystemProgram.createAccount(
+        {
+            fromPubkey: keypair.publicKey,
+            newAccountPubkey: mintKey.publicKey,
+            space: MINT_SIZE,
+            lamports: await getMinimumBalanceForRentExemptMint(connection),
+            programId: TOKEN_2022_PROGRAM_ID
+        }
+    );
+
+    const mintInstrust = createInitializeMintInstruction(
+        mintKey.publicKey,
+        9, 
+        keypair.publicKey,
+        keypair.publicKey,
+        TOKEN_2022_PROGRAM_ID
+    )
+
+    const transaction = new Transaction().add(
+        createAccountInstruction,
+        mintInstrust
+    );
+
+    const signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [keypair, mintKey]
+    );
+    let end = +new Date();
+        console.log(`Time to run: ${end - start}`);
+
+
+    console.log("Mint Address: ", mintKey.publicKey.toBase58());
+    console.log("Transaction Signature: ", signature);
+    
+}
+
