3ca4581d676a0f099fb9fa53ad5148d571c4c67e
Elly Aster <asterelly@gmail.com>
2025-07-02 17:02:29 -0400

    added files for spl_mint & init

--- solana-starter/ts/cluster1/spl_init.ts
+++ solana-starter/ts/cluster1/spl_init.ts
@@ -1,6 +1,7 @@
-import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
+import { Keypair, Connection, Commitment, SystemProgram, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
+import { createMint, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token';
 import wallet from "./wallet/turbin-wallet.json"
+
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -13,6 +14,60 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
     try {
         // Start here
         // const mint = ???
+
+        //create mint address
+        const mintKey = Keypair.generate();
+
+        const connection = new Connection("https://api.devnet.solana.com");
+
+        const mint = await createMint(
+            connection,
+            keypair,
+            keypair.publicKey,
+            null,
+            9,
+
+        )
+
+        console.log(`Mint address: ${mint.toBase58()}`);
+
+        /*
+        const createAccountInstruction = SystemProgram.createAccount(
+            {
+                fromPubkey: keypair.publicKey,
+                newAccountPubkey: mintKey.publicKey,
+                space: MINT_SIZE,
+                lamports: await getMinimumBalanceForRentExemptMint(connection),
+                programId: TOKEN_2022_PROGRAM_ID
+            }
+        );
+
+        const mintInstrust = createMint(
+            connection,
+            keypair,
+            keypair.publicKey,
+            keypair.publicKey,
+            9, 
+            mintKey
+        )
+
+      
+
+        const transaction = new Transaction().add(
+            createAccountInstruction,
+            mintInstrust
+        );
+
+        const signature = await sendAndConfirmTransaction(
+            connection,
+            transaction,
+            [keypair, mint]
+        );
+
+        console.log("Mint Address: ", mint.publicKey.toBase58());
+        console.log("Transaction Signature: ", signature);
+        */
+       
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
